4a743168f70460b7ee936126433e5d7324f405e1
Udit Manav <uditmanav17@gmail.com>
2020-04-26 13:42:22 +0530

    added add function

--- abc.py
+++ abc.py
@@ -1,6 +1,10 @@
 import itertools as it
 import collections as coll
 
+def add(x, y):
+    return x+y
+    
+    
 print("Hi there general Kenobi!")
 print(f"permutation of (1, 2, 3) are - {list(it.permutations((1, 2, 3), 3))}")
 
