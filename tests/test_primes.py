f132c7d5aafefd3dcb35d790002b6c65435e5584
Udit Manav <uditmanav17@gmail.com>
2021-08-24 16:10:39 +0530

    moved pre-commit to .githooks

--- tests/test_primes.py
+++ tests/test_primes.py
@@ -6,7 +6,7 @@ from arg_test import get_primes
     "num1, num2, expected_primes",
     [
         (1, 10, [2, 3, 5, 7]),
-        # (1, 10, [2, 3, 5, 9]), # invalid test/breaking test
+        # (1, 10, [2, 3, 5, 9]),  # invalid test/breaking test
         (10, 30, [11, 13, 17, 19, 23, 29]),
         (20, 50, [23, 29, 31, 37, 41, 43, 47]),
     ],
